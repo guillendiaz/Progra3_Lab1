8e1297a499eee42c0a1cfe5f928262220b77330e
guillendiazd <guillendiazd@unitec.edu>
2017-07-28 16:16:13 -0600

    Cuarta Actualizacion

--- Lab1_DavidGuillen.cpp
+++ Lab1_DavidGuillen.cpp
@@ -30,7 +30,7 @@ int main(){
 				double angulo;
 				cout<<"Ingrese Ángulo: ";
 				cin>>angulo;
-				cout<<angulo<<" Valor en Seno: "<<Ejercicio2Seno(angulo)<<"Valor en Coseno: "<<Ejercicio2Coseno(angulo)<<endl;
+				cout<<angulo<<" Valor en Seno: "<<Ejercicio2Seno(angulo)<<" Valor en Coseno: "<<Ejercicio2Coseno(angulo)<<endl;
 						
 		}//switch
 	}while(opcion != 4);
@@ -60,35 +60,29 @@ double Ejercicio1(int x1, int x2, int y1, int y2){
 		return valor;
 }//Ejercicio1()
 
-int FuncionSenoNum(int numero){
-	int ValNum = -1;
-	for(int i=0 ; i <= numero ; i++){
-		ValNum*= -1;
-	}//for
-	return ValNum;
-}//FuncionSenoNum()
-
-int FuncionSenoFac(int numero){
+int FuncionFac(int numero){
 	if(numero == 0){
 		return 1;
 	}else{
-		return numero * FuncionSenoFac(numero-1);
+		return numero * FuncionFac(numero-1);
 	}//ifelse
-}//FuncionSenoFac()
+}//FuncionFac()
 
-double FuncionSenoMul(double angulo, int exp){
-	double valor = 0;
-	for(int i=0 ; i <= exp ; i++){
-		valor*= angulo;
-	}//for
-	return valor;
-}//FuncionSenoMul()
-
-double Ejercicio2(double angulo){
-	double radian = (angulo / 180) * 3.141526;
+double Ejercicio2Seno(double angulo){
+	double radian = (angulo / 180) * 3.141592;
 	double valor = 0;
 	for(int i=0 ; i <= 20 ; i++){
-		valor+= (pow(-1, i) / FuncionSenoFac((2*i)+1)) * pow(radian, ((2*i)+1));
+		valor+= (pow(-1, i) / FuncionFac((2*i)+1)) * pow(radian, ((2*i)+1));
 	}//for
 	return valor;
-}//Ejercicio2()
+}//Ejercicio2Seno()
+
+double Ejercicio2Coseno(double angulo){
+	double radian = (angulo / 180) * 3.141592;
+	int valor=0;
+	for(int i = 0 ; i<= 20 ; i++){
+		valor+= (pow(-1, i) / FuncionFac((2*i))) * pow(radian, (2*i));
+		cout<<valor;
+	}//for
+	return valor;
+}//Ejercicio2Coseno
